8a546fbb79bfc1ab582b22831f3cb65e1ca75c73
MisterPaper <adielroytgarts@gmail.com>
2024-04-05 00:36:27 -0400

    meow

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.post("/api/user/login", (req, res) => {
             };
             
             let token = jwt.sign(payload, jwtOptions.secretOrKey);
-        res.json({ "message": "login successful"});
+        res.json({ "message": "login successful", "token": token});
     }).catch(msg => {
         res.status(422).json({ "message": msg });
     });
